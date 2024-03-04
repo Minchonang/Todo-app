6c6454dc4e54e3000b877bdaf0dc6e9389582bc3
minchonang <green8085@naver.com>
2024-03-05 00:30:29 +0900

    Native로 todo 앱 만들기

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+	StyleSheet,
+	Text,
+	TouchableHighlight,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { theme } from "./color";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<View style={styles.header}>
+				<TouchableOpacity activeOpacity={0}>
+					<Text style={styles.btnText}>일하고</Text>
+				</TouchableOpacity>
+				<TouchableHighlight
+					underlayColor="#ddd"
+					activeOpacity={0}
+					onPress={() => {
+						console.log("pressed");
+					}}
+				>
+					<Text style={styles.btnText}>여행하고</Text>
+				</TouchableHighlight>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: theme.bg,
+		paddingHorizontal: 20,
+	},
+	header: {
+		justifyContent: "space-between",
+		flexDirection: "row",
+		marginTop: 100,
+	},
+	btnText: {
+		fontSize: 35,
+		// color: theme.gray,
+		color: "white",
+		fontWeight: "600",
+	},
 });
